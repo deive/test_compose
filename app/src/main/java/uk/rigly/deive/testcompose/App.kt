935d3893d9763e87b654ea26fef78abf92d90b9e
deive <deive@deive.co>
2022-06-15 21:15:52 +0100

    Adds navigation animations.

--- app/src/main/java/uk/rigly/deive/testcompose/App.kt
+++ app/src/main/java/uk/rigly/deive/testcompose/App.kt
@@ -1,15 +1,19 @@
 package uk.rigly.deive.testcompose
 
 import android.os.Bundle
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import uk.rigly.deive.testcompose.address.details.Address
@@ -19,10 +23,11 @@ import uk.rigly.deive.testcompose.theme.TestComposeTheme
 import java.nio.ByteBuffer
 import java.util.*
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun App() {
     TestComposeTheme {
-        val navController = rememberNavController()
+        val navController = rememberAnimatedNavController()
         val viewModel: MainViewModel = viewModel()
         val addresses by viewModel.db.addressDao().getAll().collectAsState(initial = emptyList())
         LaunchedEffect(true) {
@@ -33,15 +38,31 @@ fun App() {
                 viewModel.db.addressDao().insertAll(response)
             }
         }
-        NavHost(
+        AnimatedNavHost(
             navController,
             "list") {
-            composable("list") {
+            composable("list",
+                enterTransition = {
+                    when (initialState.destination.route) {
+                        "detail/{id}" ->
+                            slideIntoContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(700))
+                        else -> EnterTransition.None
+                    }
+                },
+                exitTransition = {
+                    slideOutOfContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(700))
+                }) {
                 AddressItemsScreen(addresses) {
                     navController.navigate("detail/${it.uuid}")
                 }
             }
-            composable("detail/{id}") {  entry ->
+            composable("detail/{id}",
+                enterTransition = {
+                    slideIntoContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(700))
+                },
+                exitTransition = {
+                    slideOutOfContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(700))
+                }) {  entry ->
                 argument("id") {
                     type = UUIDType
                 }
